41594f772293fbecb3ff9336a6d790137502346d
SeanQO <seanquintero3@gmail.com>
2023-02-11 14:00:58 -0500

    reformated csv data

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import pandas as pd
 
 paths = {
-  "Caqueta": 'src/data/Caqueta_data.csv',
-  "Narino": 'src/data/Narino_data.csv',
-  "putumayo": 'src/data/Putumayo_data.csv'
+  "Caqueta": 'data/Caqueta_data.csv',
+  "Narino": 'data/Narino_data.csv',
+  "putumayo": 'data/Putumayo_data.csv'
 }
 
 df = pd.read_csv(paths["Caqueta"])
+
+print(df.info())
