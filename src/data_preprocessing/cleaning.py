d6e6301cd72102e44add70217817e1170c69534c
isabellaortiz5 <isabellaortiz5@yahoo.com>
2023-03-26 19:08:23 -0500

    Update cleaning.py

--- src/data_preprocessing/cleaning.py
+++ src/data_preprocessing/cleaning.py
@@ -1,66 +1,194 @@
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 from sklearn.experimental import enable_iterative_imputer
 from sklearn.impute import IterativeImputer
 from sklearn import linear_model
 import missingno as msno
 
-paths = {
-    "caqueta": '../../data/raw_2/caqueta_data_2.csv',
-    "narino": '../../data/raw_2/Narino_data_2.csv',
-    "putumayo": '../../data/raw_2/Putumayo_data_2.csv',
-    "clean_data": '../../data/processed/cleaned_data/Cleaned_data.csv',
-}
 
-common_drops = [
-    "Año", "Mes", "Programa", "Evento", "Afiliados", "OrigenBD", "DesDepto", "CodMpio", "DescMpio", "Latitud_Y_Mpio",
-    "Longitud_X_Mpio", "tipo_usuario", "Estado", "tipo_identifiCAcion", "Documento", "ConCAtenar", "nombre1", "nombre2",
-    "apellido1", "apellido2", "FechaNac", "CiclosV", "DescrCiclosV", "QuinQ", "DescQuinQ", "EnfoqueDif",
-    "Hecho Victimizante", "RUV", "Nivel_Educativo", "Ocupación", "Tipo de afiliado", "Estado_Civil", "Discapacidad",
-    "Grado de Discapacidad", "MUNICIPIO DONDE VIVE", "DIRECCIÓN DE DONDE VIVE", "TELEFONOS DE CONTACTO", "Zona",
-    "Cód_poblado", "Nombre_poblado", "Latitud_Afiliado", "Longitud_Afiliado", "Validación_Dirección_Afiliado",
-    "CodDepto_IPS", "DesDepto_IPS", "CodMpio_IPS", "DescMpio_IPS", "CodIPS", "Nombre_IPS", "Dirección_IPS",
-    "Barrio_IPS",
-    "Teléfono_IPS", "Latitud_IPS", "Longitud_IPS", "CONSUMO DE TABACO",
-    "EL USUARIO CUENTA CON ATENCIÓN POR PARTE DEL EQUIPO MULTIDISCIPLINARIO DE LA SALUD ", "CONTROLADO/NO CONTROLADO",
-    "PACIENTES CON 5 Ó MÁS MDCTOS FORMULADOS AL MES", "FECHA DE ENTREGA DE MEDICAMENTOS",
-    "MODALIDAD DE ENTREGA DE MEDICAMENTOS", "ENTREGA DE MEDICAMENTO OPORTUNA",
-    "PACIENTE I10X Y E119 QUE NO RECIBEN TRAMIENTO", "FECHA INGRESO AL PROGRAMA", "FECHA DEL SEGUNDO CONTROL",
-    "FECHA DX CONFIRMADO DE HIPERTENSIÓN ARTERIAL", "DX CONFIRMADO DE DIABETES MELLITUS",
-    "FECHA DX CONFIRMADO DE DIABETES MELLITUS", "FECHA DIAGNÓSTICO DISLIPIDEMIAS", "TENSION SISTOLICA",
-    "TENSION DIASTOLICA", "VALORACIÓN PODOLÓGICA (PIE EN DIABETICOS) POR MÉDICO GENERAL", "REALIZA ACTIVIDAD FISICA",
-    "TABAQUISMO", "RESULTADO", "FACTORES DE RIESGO", "NUTRICIÓN", "TRABAJO SOCIAL", "MEDICINA INTERNA", "PSICOLOGIA",
-    "NEFROLOGIA", "OFTALMOLOGÍA", "ENDOCRINOLOGIA", "CARDIOLOGIA", "ELECTROCARDIOGRAMA", "NEFROPROTECCIÓN",
-    "TERAPIA SUSTITUCIÓN DIALÍTICA", "TALLER EDUCATIVO ENTREGA CARTILLAS", "FECHA_CLASIF_ERC", "FECHA DEL UROANALISIS",
-    "FECHA DE ULTIMO SEGUIMIENTO ", "ETIOLOGIA DE LA ERC", "MODALIDAD COMO SE HACE EL SEGUIMIENTO DEL PACIENTE",
-    "FECHA DE PRÓXIMO CONTROL ", "CAUSAS DE INASISTENCIA", "DISMINUYO/ AUMENTO ML"
-]
+class Cleaning:
 
-narino_putumayo_drops = [
-    "FECHA NUTRUCIÓN", "FECHA TRABAJO SOCIAL", "FECHA MEDICINA INTERNA", "FECHA PISCOLOGIA", "FECHA NEFROLOGIA",
-    "FECHA OFTALMOMOGIA", "FECHA ENDOCRINOLOGIA", "FECHA CARDIOLOGIA", "FECHA ELECTROCARDIOGRAMA",
-    "FECHA NEFROPROTECCIÓN",
-]
+    paths = {
+        "caqueta": '../../data/raw_2/caqueta_data_2.csv',
+        "narino": '../../data/raw_2/Narino_data_2.csv',
+        "putumayo": '../../data/raw_2/Putumayo_data_2.csv',
+        "clean_data": '../../data/processed/cleaned_data/Cleaned_data.csv',
+    }
 
-narino_drops = [
-    "REPETIDO"
-]
+    common_drops = [
+        "Año", "Mes", "Programa", "Evento", "Afiliados", "OrigenBD", "DesDepto", "CodMpio", "DescMpio",
+        "Latitud_Y_Mpio", "Longitud_X_Mpio", "tipo_usuario", "Estado", "tipo_identifiCAcion", "Documento", "ConCAtenar", "nombre1",
+        "nombre2", "apellido1", "apellido2", "FechaNac", "CiclosV", "DescrCiclosV", "QuinQ", "DescQuinQ", "EnfoqueDif",
+        "Hecho Victimizante", "RUV", "Nivel_Educativo", "Ocupación", "Tipo de afiliado", "Estado_Civil", "Discapacidad",
+        "Grado de Discapacidad", "MUNICIPIO DONDE VIVE", "DIRECCIÓN DE DONDE VIVE", "TELEFONOS DE CONTACTO", "Zona",
+        "Cód_poblado", "Nombre_poblado", "Latitud_Afiliado", "Longitud_Afiliado", "Validación_Dirección_Afiliado",
+        "CodDepto_IPS", "DesDepto_IPS", "CodMpio_IPS", "DescMpio_IPS", "CodIPS", "Nombre_IPS", "Dirección_IPS",
+        "Barrio_IPS", "Teléfono_IPS", "Latitud_IPS", "Longitud_IPS", "CONSUMO DE TABACO",
+        "EL USUARIO CUENTA CON ATENCIÓN POR PARTE DEL EQUIPO MULTIDISCIPLINARIO DE LA SALUD ",
+        "CONTROLADO/NO CONTROLADO",
+        "PACIENTES CON 5 Ó MÁS MDCTOS FORMULADOS AL MES", "FECHA DE ENTREGA DE MEDICAMENTOS",
+        "MODALIDAD DE ENTREGA DE MEDICAMENTOS", "ENTREGA DE MEDICAMENTO OPORTUNA",
+        "PACIENTE I10X Y E119 QUE NO RECIBEN TRAMIENTO", "FECHA INGRESO AL PROGRAMA", "FECHA DEL SEGUNDO CONTROL",
+        "FECHA DX CONFIRMADO DE HIPERTENSIÓN ARTERIAL", "DX CONFIRMADO DE DIABETES MELLITUS",
+        "FECHA DX CONFIRMADO DE DIABETES MELLITUS", "FECHA DIAGNÓSTICO DISLIPIDEMIAS", "TENSION SISTOLICA",
+        "TENSION DIASTOLICA", "VALORACIÓN PODOLÓGICA (PIE EN DIABETICOS) POR MÉDICO GENERAL",
+        "REALIZA ACTIVIDAD FISICA",
+        "TABAQUISMO", "RESULTADO", "FACTORES DE RIESGO", "NUTRICIÓN", "TRABAJO SOCIAL", "MEDICINA INTERNA",
+        "PSICOLOGIA",
+        "NEFROLOGIA", "OFTALMOLOGÍA", "ENDOCRINOLOGIA", "CARDIOLOGIA", "ELECTROCARDIOGRAMA", "NEFROPROTECCIÓN",
+        "TERAPIA SUSTITUCIÓN DIALÍTICA", "TALLER EDUCATIVO ENTREGA CARTILLAS", "FECHA_CLASIF_ERC",
+        "FECHA DEL UROANALISIS",
+        "FECHA DE ULTIMO SEGUIMIENTO ", "ETIOLOGIA DE LA ERC", "MODALIDAD COMO SE HACE EL SEGUIMIENTO DEL PACIENTE",
+        "FECHA DE PRÓXIMO CONTROL ", "CAUSAS DE INASISTENCIA", "DISMINUYO/ AUMENTO ML"
+    ]
+    narino_putumayo_drops = [
+        "FECHA NUTRUCIÓN", "FECHA TRABAJO SOCIAL", "FECHA MEDICINA INTERNA", "FECHA PISCOLOGIA", "FECHA NEFROLOGIA",
+        "FECHA OFTALMOMOGIA", "FECHA ENDOCRINOLOGIA", "FECHA CARDIOLOGIA", "FECHA ELECTROCARDIOGRAMA",
+        "FECHA NEFROPROTECCIÓN"
+    ]
+    narino_drops = [
+        "REPETIDO"
+    ]
+    putumayo_drops = [
+        "FECHA DE TOMA DE CREATININA SÉRICA", "FECHA DE TOMA DE GLICEMIA 100 MG/DL_DIC",
+        "FECHA DE TOMA DE COLESTEROL TOTAL > 200 MG/DL_DIC",
+        "FECHA DE TOMA DE LDL > 130 MG/DL_DIC", "FECHA DE TOMA DE HDL HOMBRE - 40 MG/DL Y HDL MUJER - 50 MG/DL_DIC",
+        "FECHA DE TOMA DE ALBUMINURIA/CREATINURIA", "FECHA DE TOMA DE HEMOGLOBINA GLICOSILADA > DE 7%",
+        "FECHA DE TOMA DE HEMOGRAMA",
+        "FECHA DE TOMA DE POTASIO", "FECHA DE TOMA DE MICROALBINURIA", "FECHA DE TOMA DE CREATINURIA", "OBSERVACIONES",
+        "FECHA DE TOMA DE TGD > 150 MG/DL_DIC"
+    ]
 
-putumayo_drops = [
-    "FECHA DE TOMA DE CREATININA SÉRICA", "FECHA DE TOMA DE GLICEMIA 100 MG/DL_DIC",
-    "FECHA DE TOMA DE COLESTEROL TOTAL > 200 MG/DL_DIC",
-    "FECHA DE TOMA DE LDL > 130 MG/DL_DIC", "FECHA DE TOMA DE HDL HOMBRE - 40 MG/DL Y HDL MUJER - 50 MG/DL_DIC",
-    "FECHA DE TOMA DE ALBUMINURIA/CREATINURIA", "FECHA DE TOMA DE HEMOGLOBINA GLICOSILADA > DE 7%",
-    "FECHA DE TOMA DE HEMOGRAMA",
-    "FECHA DE TOMA DE POTASIO", "FECHA DE TOMA DE MICROALBINURIA", "FECHA DE TOMA DE CREATINURIA", "OBSERVACIONES",
-    "FECHA DE TOMA DE TGD > 150 MG/DL_DIC",
+    def __init__(self, saving_path):
+        self.saving_path = saving_path
+        self.caqueta_df = None
+        self.narino_df = None
+        self.putumayo_df = None
+        self.first_caqueta_clean = None
+        self.first_putumayo_clean = None
+        self.first_narino_clean = None
+        self.df = None
+        self.df_clean = None
+
+    def read_csv(self):
+        self.caqueta_df = pd.read_csv(self.paths["caqueta"])
+        self.narino_df = pd.read_csv(self.paths["narino"])
+        self.putumayo_df = pd.read_csv(self.paths["putumayo"])
+
+    def drop_columns(self):
+        caqueta_df_clean = caqueta_df.drop(common_drops, axis=1)
+
+        narino_df_clean = narino_df.drop(common_drops, axis=1)
+        narino_df_clean = narino_df_clean.drop(narino_drops, axis=1)
+        narino_df_clean = narino_df_clean.drop(narino_putumayo_drops, axis=1)
+
+        putumayo_df_clean = putumayo_df.drop(common_drops, axis=1)
+        putumayo_df_clean = putumayo_df_clean.drop(putumayo_drops, axis=1)
+        putumayo_df_clean = putumayo_df_clean.drop(narino_putumayo_drops, axis=1)
+
+        self.first_caqueta_clean = caqueta_df_clean
+        self.first_putumayo_clean = putumayo_df_clean
+        self.first_narino_clean = narino_df_clean
+
+    def concat_dfs(self):
+        self.df = pd.concat([self.putumayo_df, self.narino_df, self.caqueta_df], axis=0)
+
+    def replace_blanks(self):
+        self.df = self.df.replace(r'^\s*$', np.nan, regex=True)
+        self.df["FechaNovedadFallecido"] = self.df["FechaNovedadFallecido"].fillna('no aplica')
+        self.df["Coomorbilidad"] = self.df["Coomorbilidad"].fillna('no aplica')
+        self.df["Condición de Discapacidad"] = self.df["Condición de Discapacidad"].fillna('no aplica')
+        self.df["Tipo de Discapacidad"] = self.df["Tipo de Discapacidad"].fillna('no aplica')
+        self.df["OTROS ANTIDIABETICOS"] = self.df["OTROS ANTIDIABETICOS"].fillna('no aplica')
+        self.df["OTROS TRATAMIENTOS"] = self.df["OTROS TRATAMIENTOS"].fillna('no aplica')
+        self.df["OTROS FARMACOS ANTIHIPERTENSIVOS"] = self.df["OTROS FARMACOS ANTIHIPERTENSIVOS"].fillna('no aplica')
+        self.df = self.df.replace("#DIV/0!", np.nan)
+        self.df = self.df.replace("#NUM!", np.nan)
+        self.df = self.df.replace("#VALUE!", np.nan)
+
+    def mice_imputation(self):
+        df_mice = self.df.filter(['CALCULO DE RIESGO DE Framingham (% a 10 años)',
+                                  'GLICEMIA 100 MG/DL_DIC',
+                                  'COLESTEROL TOTAL > 200 MG/DL_DIC',
+                                  'PERIMETRO ABDOMINAL'], axis=1).copy()
+
+        # Define MICE Imputer and fill missing values
+        mice_imputer = IterativeImputer(estimator=linear_model.BayesianRidge(), n_nearest_features=None,
+                                        imputation_order='ascending')
+
+        df_mice_imputed = pd.DataFrame(mice_imputer.fit_transform(df_mice), columns=df_mice.columns)
+
+        self.df['CALCULO DE RIESGO DE Framingham (% a 10 años)'] = df_mice_imputed[
+            'CALCULO DE RIESGO DE Framingham (% a 10 años)']
+        self.df['GLICEMIA 100 MG/DL_DIC'] = df_mice_imputed['GLICEMIA 100 MG/DL_DIC']
+        self.df['COLESTEROL TOTAL > 200 MG/DL_DIC'] = df_mice_imputed['COLESTEROL TOTAL > 200 MG/DL_DIC']
+        self.df['PERIMETRO ABDOMINAL'] = df_mice_imputed['PERIMETRO ABDOMINAL']
+
+    @staticmethod
+    def get_nan_per_col(df):
+        nan_percentage = ((df.isna().sum() * 100) / df.shape[0]).sort_values(ascending=True)
+        return nan_percentage
+
+    @staticmethod
+    def remove_by_nan(accepted_nan_percentage, nan_percentaje_series, df):
+        columns_droped = []
+        for items in nan_percentaje_series.iteritems():
+            if items[1] > accepted_nan_percentage:
+                df = df.drop([items[0]], axis=1)
+                columns_droped.append(items)
+
+        return df, columns_droped
+
+    def drop_nan(self):
+        nan_percentages = get_nan_per_col(df)
+        self.df_clean, columns_droped = remove_by_nan(5, nan_percentages, df)
+        self.df_clean = self.df_clean.dropna()
+
+    def save_df(self):
+        self.df_clean = self.df_clean.reset_index(drop=True)
+        self.df_clean.to_csv(paths["clean_data"])
+
+    def run(self):
+        self.read_csv()
+        self.drop_columns()
+        self.concat_dfs()
+        self.replace_blanks()
+        self.mice_imputation()
+        self.drop_nan()
+        self.get_nan_per_col()
+        self.save_df()
+
+    #gets
+
+    def get_caqueta_df(self):
+        return self.caqueta_df
+
+    def get_narino_df(self):
+        return self.narino_df
+
+    def get_putumayo_df(self):
+        return self.putumayo_df
+
+    def get_first_caqueta_clean(self):
+        return self.first_caqueta_clean
+
+    def get_first_narino_clean(self):
+        return self.first_narino_clean
+
+    def get_first_putumayo_clean(self):
+        return self.first_putumayo_clean
+
+    def get_df(self):
+        return self.df
+
+    def get_df_clean(self):
+        return self.df_clean
 
 
-]
 
-"""
-****************************** read csv and make drops ******************************
+
+
+
 """
 caqueta_df = pd.read_csv(paths["caqueta"])
 narino_df = pd.read_csv(paths["narino"])
@@ -91,9 +219,7 @@ print(narino_df_clean.info())
 print("*****************putumayo*****************")
 print(putumayo_df_clean.info())
 
-"""
-****************************** concatenar dropped csv ******************************
-"""
+
 df = pd.concat([putumayo_df_clean, narino_df_clean, caqueta_df_clean], axis=0)
 print("***************** unified df")
 print(df.info())
@@ -102,9 +228,7 @@ msno.matrix(df);
 plt.title("Missingness - unified data frame")
 plt.show()
 
-"""
-****************************** blank to no aplica ******************************
-"""
+
 df = df.replace(r'^\s*$', np.nan, regex=True)
 df["FechaNovedadFallecido"] = df["FechaNovedadFallecido"].fillna('no aplica')
 df["Coomorbilidad"] = df["Coomorbilidad"].fillna('no aplica')
@@ -118,9 +242,7 @@ df = df.replace("#DIV/0!", np.nan)
 df = df.replace("#NUM!", np.nan)
 df = df.replace("#VALUE!", np.nan)
 
-"""
-****************************** plt of unified and filled data ******************************
-"""
+
 # Plot missingness of unified data
 print("********* UNIFIED DF ********")
 print(df.info())
@@ -155,7 +277,7 @@ plt.title("Missingness Data after imputation")
 plt.show()
 
 def get_nan_per_col(df):
-    """
+    
       Iterates trough every column and caculates the percentage of nan values present in each column.
 
       :param df: {pandas data frame}.
@@ -163,13 +285,13 @@ def get_nan_per_col(df):
       :return: a series of all the nan percentages asociated to the column name.
       :rtype: series.
 
-      """
+  
     NAN_percentage = ((df.isna().sum() * 100) / df.shape[0]).sort_values(ascending=True)
     return NAN_percentage
 
 
 def removeByNan(accepted_nan_percentage, nan_percentaje_series, df):
-    """
+    
       Takes a series that contains all the columns name with the respective nan percentaje,
       then drops the columns that had nan content above the diven max nan limit.
 
@@ -184,7 +306,7 @@ def removeByNan(accepted_nan_percentage, nan_percentaje_series, df):
       :return columns_droped: list of the name of the droped columns and its nan percentage.
       :rtype columns_droped: list[str,float].
 
-      """
+
     columns_droped = []
     for items in nan_percentaje_series.iteritems():
         if items[1] > accepted_nan_percentage:
@@ -201,7 +323,7 @@ print("****************************** nans drops ******************************"
 print(NAN_percentages)
 print(columns_droped)
 
-""" **************************************** """
+
 df_clean = df_clean.dropna()
 df_clean = df_clean.reset_index(drop=True)
 df_clean.to_csv(paths["clean_data"])
@@ -218,7 +340,7 @@ plt.boxplot(df_clean['CALCULO DE RIESGO DE Framingham (% a 10 años)'])
 plt.title('Distribucion de riesgo de Framingham')
 plt.show()
 
-"""
+
 #Histograma de edad
 
 df_clean['Edad'].hist(bins=5)
@@ -227,7 +349,7 @@ plt.xlabel('Edad')
 plt.ylabel('Cuenta')
 plt.show()
 
-"""
+
 #bar chart género
 gender_counts = df['Cod_Género'].value_counts()
 ax = gender_counts.plot(kind='bar')
@@ -246,8 +368,7 @@ plt.ylabel('Número de personas')
 ax.set_xticklabels(['No fuma', 'Fuma'])
 plt.show()
 
-"""
-#pie chart life cycle
+
 
 cycle_counts = df_clean['CiclosV'].value_counts()
 cycle_percentages = cycle_counts / cycle_counts.sum() * 100
@@ -259,5 +380,6 @@ labels = [f'{i}: {j:.1f}%' for i, j in zip(cycle_percentages.index, cycle_percen
 plt.legend(bbox_to_anchor=(0.85, 1), loc='upper left',labels=labels)
 plt.show()
 
-"""
 
+
+"""
