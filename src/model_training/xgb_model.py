98d8f403a80d5e2dc4fe82274077ddf036f5e5c1
isabellaortiz5 <isabellaortiz5@yahoo.com>
2023-06-03 15:32:50 -0500

    xgboost plots

--- src/model_training/xgb_model.py
+++ src/model_training/xgb_model.py
@@ -1,117 +1,15 @@
 import xgboost as xgb
 from sklearn.model_selection import GridSearchCV
-from sklearn.metrics import accuracy_score, classification_report
-from sklearn.preprocessing import LabelEncoder
-
-
-
-"""
-class XGBModel:
-    def __init__(self, X_train, y_train, X_test, y_test, param_grid):
-        self.params = {
-            'use_label_encoder': False,
-            'eval_metric': 'mlogloss',
-            'objective': 'multi:softmax',
-        }
-        self.grid_search = None
-        self.best_model = None
-        self.X_train = X_train
-        self.y_train = self._prepare_target(y_train)
-        self.X_test = X_test
-        self.y_test = self._prepare_target(y_test)
-        self.param_grid = param_grid
-        self.y_pred = None
-        self.params['num_class'] = len(set(self.y_train))
-
-    def _prepare_target(self, y):
-        label_encoder = LabelEncoder()
-        return label_encoder.fit_transform(y)
-
-    def hyperparameter_tuning(self, scoring='accuracy', cv=3, verbose=1):
-        model = xgb.XGBClassifier(**self.params)
-        self.grid_search = GridSearchCV(model, self.param_grid, scoring=scoring, cv=cv, verbose=verbose)
-        self.grid_search.fit(self.X_train, self.y_train)
-        self.params.update(self.grid_search.best_params_)
-        print("Best parameters: ", self.params)
-
-    def train(self):
-        self.best_model = xgb.XGBClassifier(**self.params)
-        self.best_model.fit(self.X_train, self.y_train)
-
-    def evaluate(self):
-        self.y_pred = self.best_model.predict(self.X_test)
-
-        accuracy = accuracy_score(self.y_test, self.y_pred)
-        print("Accuracy: %.2f%%" % (accuracy * 100))
-
-        print("Classification Report:\n", classification_report(self.y_test, self.y_pred))
-
-    def run(self):
-        self.hyperparameter_tuning()
-        self.train()
-        self.evaluate()
-
-
-        class XGBoostModel:
-    def __init__(self, n_estimators=100, learning_rate=0.1, max_depth=3):
-        self.model = xgb.XGBClassifier(n_estimators=n_estimators, learning_rate=learning_rate, max_depth=max_depth)
-
-    def fit(self, X, y):
-        self.model.fit(X, y)
-
-    def predict(self, X):
-        return self.model.predict(X)
-
-    # Train and evaluate model
-    model = XGBoostModel(n_estimators=200, learning_rate=0.05, max_depth=5)
-    model.fit(X_train, y_train)
-    predictions = model.predict(X_test)
-
-    # Calculate accuracy of the model
-    accuracy = np.sum(predictions == y_test) / len(y_test)
-    print("Accuracy:", accuracy)
-
-
-    import xgboost as xgb
-from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import accuracy_score
-
-
-
-"""
-
-"""
-class XGBModel:
-    def __init__(self, X_train, y_train, param_grid):
-        self.X_train = X_train
-        self.y_train = y_train
-        self.param_grid = param_grid
-
-    def run(self):
-        xgb_model = xgb.XGBClassifier()
-        grid_search = GridSearchCV(xgb_model, self.param_grid, cv=5)
-        grid_search.fit(self.X_train, self.y_train)
-        best_params = grid_search.best_params_
-
-        # Training the model with the best hyperparameters
-        xgb_model = xgb.XGBClassifier(**best_params)
-        xgb_model.fit(self.X_train, self.y_train)
-
-        # Predicting on the test set
-        y_pred = xgb_model.predict(self.X_test)
-
-        # Printing the accuracy of the model
-        accuracy = (y_pred == self.y_test).mean()
-        print("Accuracy:", accuracy)
-"""
-
-from xgboost import XGBClassifier
-from sklearn.metrics import accuracy_score
-from sklearn.model_selection import GridSearchCV
 from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import confusion_matrix
 import numpy as np
 from joblib import dump
 import os
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from xgboost import XGBClassifier
 
 class XGBModel:
     def __init__(self, X_train, y_train, X_test, X_val, y_test, y_val):
@@ -154,9 +52,9 @@ class XGBModel:
 
     def test(self):
         y_pred = self.xgb_model.predict(self.X_test)
-
         accuracy = accuracy_score(self.y_test, y_pred)
         print("Accuracy score: {:.2f}".format(accuracy))
+        return y_pred
 
     def tune_hyperparameters(self):
         xgb_classifier = XGBClassifier(n_jobs=-1, early_stopping_rounds=10)
@@ -164,7 +62,23 @@ class XGBModel:
         grid_search.fit(self.X_train, self.y_train, eval_set=[(self.X_val, self.y_val)], verbose=False)
         self.xgb_model = grid_search.best_estimator_
         print("Best hyperparameters: {}".format(grid_search.best_params_))
+        return grid_search.cv_results_
+    
+    def plot_hyperparameters(self, cv_results):
+        cv_results_df = pd.DataFrame(cv_results)
+        param_columns = [column for column in cv_results_df.columns if column.startswith('param_')]
+        for column in param_columns:
+            cv_results_df[column] = cv_results_df[column].astype(float)
 
+        for param in self.param_grid.keys():
+            plt.figure(figsize=(10, 6))
+            sns.heatmap(cv_results_df.pivot('param_'+param, 'rank_test_score', 'mean_test_score'),
+                        annot=True, cmap="YlGnBu", cbar=True)
+            plt.title('Hyperparameters tuning')
+            plt.xlabel('Rank')
+            plt.ylabel(param)
+            plt.show()
+    
     def train(self):
         # Set the number of threads to the number of cores for XGBoost
         params = {'n_jobs': -1}
@@ -180,10 +94,11 @@ class XGBModel:
 
     def run(self):
         self.train()
-        self.test()
-        self.tune_hyperparameters()
+        y_pred = self.test()
+        cv_results = self.tune_hyperparameters()
+        self.plot_hyperparameters(cv_results)
         self.train()
-        self.test()
+        y_pred = self.test()
 
         y_pred = self.xgb_model.predict(self.X_test)
         return y_pred, self.xgb_model
